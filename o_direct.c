69cbb3ae1f2206f22ee84039d30369415b7f92d8
Azat Khuzhin <a3at.mail@gmail.com>
2015-02-25 13:34:35 +0300

    Wrap fopen() too, since it don't call open() that we overwrites
    This implementation is leaky (one fd per fopen()), and also have mode =
    0600 for files (always).

--- o_direct.c
+++ o_direct.c
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <stdarg.h>
+#include <stdio.h>
 
 
 int (*open_orig)(const char *pathname, int flags, mode_t mode) = NULL;
@@ -25,4 +26,50 @@ int open(const char *pathname, int flags, ...)
         va_end(arg);
     }
     return open_orig(pathname, flags, mode);
+}
+
+static int posix_flags(const char *mode)
+{
+    int flags = 0;
+    int i;
+
+    switch (*mode) {
+        case 'r':
+            flags = O_RDONLY;
+            break;
+        case 'w':
+            flags = O_WRONLY;
+            flags |= (O_CREAT|O_TRUNC);
+            break;
+        case 'a':
+            flags = O_WRONLY;
+            flags |= (O_CREAT|O_APPEND);
+            break;
+    }
+
+    for (i = 0; i < 7 && *++mode; ++i) {
+        switch (*mode) {
+            case '+':
+                flags |= O_RDWR;
+                continue;
+            case 'x':
+                flags |= O_EXCL;
+                continue;
+            case 'e':
+                flags |= O_CLOEXEC;
+                continue;
+        }
+    }
+
+    return flags;
+}
+FILE *fopen(const char *path, const char *mode)
+{
+    int pflags = posix_flags(mode);
+    /**
+     * XXX: leaked fd, and custom @mode
+     */
+    int fd = open(path, pflags, 0600);
+    FILE *f = fdopen(fd, mode);
+    return f;
 }
